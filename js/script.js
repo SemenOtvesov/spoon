2001dc8dffa820c370b14809e6c2b53bf310ae5a
SemenOtvesov <99428817+SemenOtvesov@users.noreply.github.com>
2022-05-18 19:01:21 +0300

    Delete js directory

--- js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-	$('.header__burger').click(function (event) {
-		$('.header__burger, .header__menu, .header').toggleClass('active');
-		$('body').toggleClass('lock');
-	});
-	$('.info__title').click(function (event) {
-		$(this).toggleClass('active-title').next().slideToggle(300);
-	});
-});
